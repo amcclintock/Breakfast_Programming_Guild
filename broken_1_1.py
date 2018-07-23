aa2e1cb1ea207ae727aa2bc3e8cfabbc82f93622
Aaron McClintock <amcclintock@spscommerce.com>
2018-07-23 13:45:06 -0500

    1.1 Initial File

---
+++ broken_1_1.py
@@ -0,0 +1,18 @@
+def even_or_odd(num):
+    print('\t...starting a function called \'am_I_Even\'')
+    if num % 2 == 0: # yay using a mod!
+        return True
+    else:
+        return True
+        print('This is not true')
+
+#Start by collecting a number
+number = int(input('Enter a number:'))
+
+if am_I_Even(numbr) is True:
+    print number, 'is an even number'
+else:
+    print number, 'is an odd number'
+
+    #Always finish with statement to confirm completed
+    print('\n\tCheck completed, well done')
